b58c4e6d8f0678823b053411467a189d09ba0361
Kamirii <camilyalbres@gmail.com>
2022-09-11 14:30:13 -0300

    pequenas refatorações

--- linxImpulse/src/components/Header/header.jsx
+++ linxImpulse/src/components/Header/header.jsx
@@ -1,26 +1,24 @@
 import styles from "./header.module.css";
-import {Button} from "../button/button.jsx";
+import { Button } from "../button/button.jsx";
 
 export const Header = () => {
-    return (
+  return (
 
-        <header className={styles.header}>
-        <div>
+    <header className={styles.header}>
+      <div>
         <h4>Uma seleção de produtos</h4>
         <h1>especial para você</h1>
-        <p>
-          Todos os produtos desta lista foram selecionados a partir da sua navegação. Aproveite!
-        </p>
-        </div>
+        <p>Todos os produtos desta lista foram selecionados a partir da sua navegação. Aproveite!</p>
+      </div>
 
-         <div  className={styles.navbtn}>
-          <a href="#">Conheça a Linx</a>
-          <a href="#"> Ajude o algoritmo</a>
-          <a href="#">Seus produtos</a>
-          <a href="#">Compartilhe</a>
-        </div>
+      <div className={styles.navbtn}>
+        <a href="#">Conheça a Linx</a>
+        <a href="#"> Ajude o algoritmo</a>
+        <a href="#">Seus produtos</a>
+        <a href="#">Compartilhe</a>
+      </div>
 
-        
-      </header>
-      );
-    };
+
+    </header>
+  );
+};
--- linxImpulse/src/components/form/form.jsx
+++ linxImpulse/src/components/form/form.jsx
@@ -1,16 +1,14 @@
 import styles from "./form.module.css"
-import {Button} from "../button/button.jsx"
+import { Button } from "../button/button.jsx"
 export const Form = () => {
     return (
-        <section className={styles.section}>
+        <section className={styles.container}>
 
-
-            <div className={styles.sectionText}>
+            <div className={styles.containerText}>
                 <h1>Ajude o algoritmo a ser mais certeiro</h1>
                 <p>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ultricies tellus nec mi porta convallis sollicitudin eu urna. Mauris rhoncus erat sed interdum dignissim. Suspendisse semper pretium consectetur. Praesent bibendum arcu risus, sit amet iaculis ex tempus quis. Cras et erat ut tellus vulputate tincidunt. Aenean lacinia euismod augue vel egestas. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum vel urna tortor. Vivamus et arcu non felis tristique eleifend. Morbi eu condimentum urna. Curabitur eu magna eget turpis condimentum ultrices. Suspendisse quis lorem ultricies, pulvinar purus sed, egestas erat. Etiam ultricies a ante vehicula pharetra. Quisque ut neque mattis, consequat velit ut, ultrices orci. Nulla varius elementum erat vel pharetra. Aenean sit amet nisi diam. Morbi viverra, magna ac luctus commodo, odio ante suscipit libero, at mattis augue est vel metus.
                 </p>
-                <p></p>
                 <p>
                     Morbi eu condimentum urna. Curabitur eu magna eget turpis condimentum
                     ultrices. Suspendisse quis lorem ultricies, pulvinar purus sed,
@@ -22,8 +20,7 @@ export const Form = () => {
                 </p>
             </div>
 
-
-            <form className={styles.sectionForm}>
+            <form className={styles.containerForm}>
                 <div className={styles.formInput}>
                     <label>
                         Nome:
@@ -40,16 +37,11 @@ export const Form = () => {
                 </div>
 
                 <div className={styles.formCheckboxes}>
-                    <label htmlFor="masculino" >
-                        <input type="radio"/>
-                        Masculino
-                    </label>
-                    <label htmlFor="feminino">
-                        <input type="radio" />
-                        Feminino
-                    </label>
+                    <input type="radio" name="sexo" id="masculino" />
+                    <label htmlFor="masculino">masculino</label>
+                    <input type="radio" name="sexo" id="feminino" />
+                    <label htmlFor="feminino">feminino</label>
                 </div>
-
                 <Button type="submit" value="Enviar"></Button>
             </form>
 
