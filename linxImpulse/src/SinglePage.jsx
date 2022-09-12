4b605fb209bd73b28fd67445db03c48928e28966
Kamirii <camilyalbres@gmail.com>
2022-09-12 15:49:05 -0300

    footer funcionando

--- linxImpulse/src/SinglePage.jsx
+++ linxImpulse/src/SinglePage.jsx
@@ -3,7 +3,7 @@ import { Header } from "./components/Header/header.jsx"
 import { Form } from "./components/Form/form.jsx"
 import {Divider} from "./components/divider/divider.jsx"
 import {FormShare} from "./components/FormShare/formShare.jsx"
-
+import {Footer} from "./components/Footer/footer.jsx"
 
 function SinglePage() {
   const [count, setCount] = useState(0)
@@ -15,7 +15,10 @@ function SinglePage() {
     <Divider text="Sua seleção especial" />
     <Form/>
     <Divider text="Compartilhe a novidade" />
+    <Form/>
     <FormShare/>
+    <Footer/>
+    
    </div>
    
   )
---
+++ linxImpulse/src/components/Footer/footer.jsx
@@ -0,0 +1,14 @@
+import styles from "./footer.module.css"
+
+export const Footer = () => { 
+    return(
+       
+        <footer className={styles.footer}>
+        <p>Testando suas habilidades em ReactJS</p>
+        <p>Resilia Educação</p>
+        <p>2022</p>
+    </footer>
+
+     )
+   
+}
