4fb11af7f96cfc016d30f96a79781a358a7f0bea
Kamirii <camilyalbres@gmail.com>
2022-09-13 00:57:32 -0300

    api e layout funcional

--- linxImpulse/src/SinglePage.jsx
+++ linxImpulse/src/SinglePage.jsx
@@ -1,27 +1,55 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Header } from "./components/Header/header.jsx"
 import { Form } from "./components/Form/form.jsx"
-import {Divider} from "./components/divider/divider.jsx"
-import {FormShare} from "./components/FormShare/formShare.jsx"
-import {Footer} from "./components/Footer/footer.jsx"
+import { Divider } from "./components/divider/divider.jsx"
+import { ProductLayout } from "./components/product/productLayout"
+import { FormShare } from "./components/FormShare/formShare.jsx"
+import { Footer } from "./components/Footer/footer.jsx"
+import styles from "./components/product/product.module.css"
 
 function SinglePage() {
-  const [count, setCount] = useState(0)
+
+  const [product, setProduct] = useState([])
+  const [page, setPage] = useState(1)
+  const url = `https://frontend-intern-challenge-api.iurykrieger.vercel.app/products?page=${page}`
+
+  const getApi = async () => {
+
+      let res = await fetch(url).then((data) => data.json());
+      setPage(page + 1);
+      setProduct([...product, ...res.products]);
+      console.log(res)
+  };
+  useEffect(() => {
+    getApi();
+  }, []);
 
   return (
-  <div> 
-   <Header/>
-    <Form/>
-    <Divider text="Sua seleção especial" />
-    <Form/>
-    <Divider text="Compartilhe a novidade" />
-    <Form/>
-    <FormShare/>
-    <Footer/>
-    
-   </div>
-   
-  )
+    <div>
+      <Header />
+      <Form />
+      <Divider text="Sua seleção especial" />
+      
+      <div className={styles.card}>
+      {product.map(produto => {
+        return (
+          <ProductLayout
+            key={produto.id}
+            image={produto.image}
+            name={produto.name}
+            description={produto.description}      
+            oldPrice={`De: R$${produto.oldPrice},00`}
+            specialOffer={`Por: R$${produto.price},00`}
+            installments={`ou ${produto.installments.count}x de R$${produto.installments.value}0`} 
+            />   
+        )
+      })}
+      </div>  
+      <Divider text="Compartilhe a novidade" />
+      <FormShare />
+      <Footer />
+    </div>
+ )
 }
 
 export default SinglePage
---
+++ linxImpulse/src/components/product/productLayout.jsx
@@ -0,0 +1,22 @@
+import styles from "./product.module.css"
+import {Button} from "../button/button.jsx"
+export const ProductLayout = (props) => {
+    return (
+        <div className={styles.mainContainer}>
+            <div className = {styles.imageContainer}>
+                <img src={props.image}></img>
+                </div>
+                <div className = {styles.infoContainer}>
+                <p>{props.name}</p>
+                <p>{props.description}</p>
+                <p>{props.oldPrice}</p>
+                <strong>{props.specialOffer}</strong>
+                <p>{props.installments}</p>
+                </div>  
+                <a href="#" className={styles.buttonProduct}>Comprar</a>
+                
+            </div>
+        
+    )
+
+}
